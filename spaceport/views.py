eb74e822ea4ec7f8ec3f40a06b390e8c375502c0
MojosBeans100 <lucy.colley@btinternet.com>
2022-01-17 10:39:41 +0000

    Link up urls to spaceport urls, create first view to render homepage

--- spaceport/views.py
+++ spaceport/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect, reverse
+import requests
+
+def homepage(request):
+    return render(request, 'index.html')
