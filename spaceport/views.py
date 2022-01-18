b26cfb42d43a51982da6b58fa2a6ac711d22000b
MojosBeans100 <lucy.colley@btinternet.com>
2022-01-18 11:22:16 +0000

    In views.update, if an image is found in the result response from api, update the Result object and save

--- spaceport/views.py
+++ spaceport/views.py
@@ -247,31 +247,47 @@ def update(request, id):
 
             # create new result object
             new_result = Result(
-                pipeline_id = update_list,
-                created_at = i['created_at'],
-                updated_at = i['updated_at'],
-                api_pipeline_id = i['pipeline_id'],
-                status = i['status'],
-                output_id = i['output_id'],
-                message = i['message'],
-                interval_start_date = i['interval']['start_date'],
-                interval_end_date = i['interval']['end_date'],
-                scene_height = i['overall_metadata']['scene_height'],
-                scene_width = i['overall_metadata']['scene_width'],
-                filled_area = i['overall_metadata']['filled_area_km'],
-                aoi_area_per = i['overall_metadata']['filled_area_percentage_of_aoi'],
-                cloud_cover_per = i['overall_metadata']['cloud_cover_percentage'],
-                aoi_cloud_cover_per = i['overall_metadata']['cloud_cover_percentage_of_aoi'],
-                visible_area = i['overall_metadata']['visible_area_km2'],
-                aoi_visible_area_per = i['overall_metadata']['visible_area_percentage_of_aoi'],
+                pipeline_id=update_list,
+                created_at=i['created_at'],
+                updated_at=i['updated_at'],
+                api_pipeline_id=i['pipeline_id'],
+                status=i['status'],
+                output_id=i['output_id'],
+                message=i['message'],
+                interval_start_date=i['interval']['start_date'],
+                interval_end_date=i['interval']['end_date'],
             )
+
+        # save the newly created result
+        new_result.save()
+
+        # if an image is found for each interval
+        if len(i['results']) > 0:
+
+            new_result.image_created_at = i['results'][0]['created_at'],
+            new_result.image_updated_at = i['results'][0]['updated_at'],
+            new_result.image_preview_url = i['results'][0]['preview_url'],
+            new_result.image_visual_url = i['results'][0]['visual_url'],
+            new_result.image_analytics_url = i['results'][0]['analytics_url'],
+            new_result.image_metadata_url = i['results'][0]['metadata_url'],
+            new_result.image_size = i['results'][0]['metadata']['size_in_mb'],
+            new_result.image_valid_pixels_per = i['results'][0]['metadata']['valid_pixels_percentage'],
+            new_result.image_source = i['results'][0]['metadata']['source'],
+            new_result.scene_height = i['overall_metadata']['scene_height'],
+            new_result.scene_width = i['overall_metadata']['scene_width'],
+            new_result.filled_area = i['overall_metadata']['filled_area_km'],
+            new_result.aoi_area_per = i['overall_metadata']['filled_area_percentage_of_aoi'],
+            new_result.cloud_cover_per = i['overall_metadata']['cloud_cover_percentage'],
+            new_result.aoi_cloud_cover_per = i['overall_metadata']['cloud_cover_percentage_of_aoi'],
+            new_result.visible_area = i['overall_metadata']['visible_area_km2'],
+            new_result.aoi_visible_area_per = i['overall_metadata']['visible_area_percentage_of_aoi'],
 
         # save the newly created result
         new_result.save()
 
     # if there are already results created for this List object
     # update them
-    else:
+    #else:
 
     return redirect(reverse('detail_view', args=[id]))
 
