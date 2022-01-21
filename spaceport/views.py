dbdfdca6d00f5655208da32e3a5823ce73888b40
MojosBeans100 <lucy.colley@btinternet.com>
2022-01-21 07:10:26 +0000

    Add Mapbox map to detail view to view AOI in more accessible way

--- spaceport/views.py
+++ spaceport/views.py
@@ -86,21 +86,19 @@ def save(request):
 def create(request):
     """
     Creates the pipeline (List) object.
-  
-    This function checks the validity of the form
-    and if valid, saves it as a List object.
 
-    It then calls the Skywatch API with user's parameters
-    and, if successful, saves additional fields into 
-    the List form, namely the api_id for cross reference.
-  
+    This function checks the validity of the form
+    and if valid, it then calls the Skywatch API with user's parameters
+    and, if successful, saves the List object and saves additional fields into
+    the List object, namely the api_id for cross reference.
+
     Parameters:
     Request - user's submitted form.
-  
+
     Returns:
     Saved List object
     Redirects to Detail View of List object.
-  
+
     """
 
     # current logged in user
@@ -217,10 +215,24 @@ def create(request):
 
 # display all the user's models
 def my_pipelines(request):
+    """
+    Displays the user's pipelines.
+  
+    This function filters all user's pipelines 
+    by status and renders them in the webpage.
+  
+    Parameters:
+    Request.
+  
+    Returns:
+    All user's pipelines.
+
+    """
 
     user = str(request.user)
 
-    active_pipelines = List.objects.filter(status='active', created_by=user).order_by('date_created')
+    active_pipelines = List.objects.filter(status='active',
+        created_by=user).order_by('date_created')
 
     complete_pipelines = List.objects.filter(status='complete',
         created_by=user).order_by('date_created')
