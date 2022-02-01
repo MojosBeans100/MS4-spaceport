87c431a1ee6c6af7f4ed6764b591ad83ade17f10
MojosBeans100 <lucy.colley@btinternet.com>
2022-02-01 13:58:16 +0000

    Add testing details and images to testing.md for user story 5

--- ms4spaceport/settings.py
+++ ms4spaceport/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = 'my_pipelines'
 LOGOUT_REDIRECT_URL = '/'
+# SIGNUP_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
